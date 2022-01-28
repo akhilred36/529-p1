0f66aeb2f1f86fa980abf021ed49e2d667aca472
MuricanWizard <herculesroxx@gmail.com>
2022-01-27 17:48:06 -0700

    Game plan

--- main.cpp
+++ main.cpp
@@ -24,20 +24,10 @@ int main(){
     //Initialize vector of string vectors for the dataframe
     vector<vector<string>> data; 
     data = read_csv("car_evaluation.csv");
-    //println("Data: ");
-    //printDataFrame(data);
-    //println("Columns: ");
     pair<vector<string>, vector<vector<string>>> seperatedData = seperateHeader(data);
     vector<string> columns = seperatedData.first;
     vector<vector<string>> dataPoints = seperatedData.second;
-    //printColumns(columns);
-    //println("\n \n \n Data points:");
-    //printDataFrame(dataPoints);
     pair<vector<vector<string>>, vector<string>> attr_targets = seperateTargets(seperatedData.second, 6); 
-    //println(" \n \n Targets: ");
-    //printColumns(attr_targets.second);
-    //println("Attributes: ");
-    //printDataFrame(attr_targets.first);
     vector<vector<string>> shuffled = shuffleDataFrame(seperatedData.second);
     //printDataFrame(shuffled);
     pair<vector<vector<string>>, vector<vector<string>>> data_split = train_test_split(shuffled, 0.8);
--- tree.h
+++ tree.h
@@ -6,13 +6,42 @@
 #include <stdexcept> // runtime_error
 #include <sstream> // stringstream
 
+using namespace std;
+
 namespace tree{
 
     class Node{
         //pointers to other nodes
         //Vector to hold node pointers
         int id;
-        std::vector<Node *> children;
-        std::string hypothesis;
+        vector<Node *> children;
+        string hypothesis;
+        vector<string> hypothesis_choices;
+        vector<vector<string>> subdataset;
+
+        // constructor 
+        // create/append child
+        // Utilize custom dataframe
     };
+
+    // creates and trains decision tree 
+    void train(vector<vector<string>> data){
+        vector<pair<string, double>> impurities;
+
+    }
+
+    // test performance (accuracy, precision, AUC)
+
+    // prune tree
+
+    // create/train RF/ensemble
+
+    // test RF
+
+    // get gini index
+
+    // Testing frameworks
+
+
 }
+
